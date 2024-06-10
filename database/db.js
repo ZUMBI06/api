bc130e73ea628c20eca10abe2efcba2eaded1597
ZUMBI06 <bastoscamargojhonatas@gmail.com>
2024-06-10 09:42:52 -0300

    config para remoto

--- database/db.js
+++ database/db.js
@@ -1,11 +1,22 @@
-import mysql from "mysql2"
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// export const db = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "admin",
+//     database: "db_exemplo"
+// })
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "admin",
-    database: "db_exemplo"
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 })
 
 db.connect((err) => {
